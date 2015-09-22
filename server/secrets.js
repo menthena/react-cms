0460520068c6fcbfd4cf7dff2e413e3c9c7b7947
Luke Marsh <luke@theappbusiness.com>
2015-09-22 12:43:09 +0100

    updating secrets file

--- server/secrets.js
+++ server/secrets.js
@@ -1,13 +1,13 @@
 var secrets = {};
 
 secrets.google = {
-	clientID: '328354766394-vpocb7doatubgb31qu5hu55p3mnk6unq.apps.googleusercontent.com',
-  clientSecret: 'rwf7sE20_wlxfnA5Ir1OR0Nu',
+	clientID: '328354766394-2v20l3ggtoo2q69qttv07btohmhv2c6j.apps.googleusercontent.com',
+  clientSecret: 'pBwgaKSwhwxe0dypxswrMpnB',
   callbackURL: 'http://localhost:3000/auth/google/callback'
 };
 
 secrets.session = {
-	secret: 'bitsinpieces'
+	secret: 'happymealsforeveryone'
 };
 
 module.exports = secrets;
