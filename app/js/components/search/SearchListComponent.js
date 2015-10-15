98a8ad4457315c606049e2e5abf0f261dbe580d5
Luke Marsh <luke@theappbusiness.com>
2015-10-15 07:30:58 +0100

    fixing broken component

--- app/js/components/search/SearchListComponent.js
+++ app/js/components/search/SearchListComponent.js
@@ -10,36 +10,55 @@ require('../../../styles/SearchListComponent.sass');
 var SearchListComponent = React.createClass({
 
   render: function () {
-    console.log('a');
-    var results = this.props.results || [];
-    var groupedResults = _.groupBy(results, 'matchType');
-
-
     return (
-      <div className="section-list-component">
-        {
-          groupedResults.map((groupedResult) => {
-            let title;
-            if (groupedResult[0].matchType === 'section') {
-              title = 'Sectionsx';
-            } else if (groupedResult[0].matchType === 'component') {
-              title = 'Components';
-            }
-
-            return (
-              <div>
-                <h3>{title}</h3>
-                <ul>
-                  {groupedResult.map((result) => {
-                    return <li>{section}</li>;
-                  })}
-                </ul>
-              </div>
-            );
-          })
-        }
-      </div>
+      '<div>a</div>'
     );
+    // console.log('a');
+    // var results = this.props.results || [];
+    // var groupedResults = [];
+    //
+    // if (results.length) {
+    //   console.log(results.length);
+    //   groupedResults = _.groupBy(results, 'matchType');
+    //   console.log(groupedResults.length);
+    //   if (groupedResults.length) {
+    //     groupedResults.map((groupedResult) => {
+    //       let title;
+    //       if (groupedResult[0].matchType === 'section') {
+    //         title = 'Sections';
+    //       } else if (groupedResult[0].matchType === 'component') {
+    //         title = 'Components';
+    //       }
+    //     });
+    //   }
+    // }
+    //
+    // return (
+    //   <div className="section-list-component">
+    //     {
+    //       groupedResults.map((groupedResult) => {
+    //         console.log(groupedResult);
+    //         let title;
+    //         if (groupedResult[0].matchType === 'section') {
+    //           title = 'Sectionsx';
+    //         } else if (groupedResult[0].matchType === 'component') {
+    //           title = 'Components';
+    //         }
+    //
+    //         return (
+    //           <div>
+    //             <h3>{title}</h3>
+    //             <ul>
+    //               {groupedResult.map((result) => {
+    //                 return <li>{section}</li>;
+    //               })}
+    //             </ul>
+    //           </div>
+    //         );
+    //       })
+    //     }
+    //   </div>
+    // );
   }
 });
 
